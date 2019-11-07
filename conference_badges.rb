b6f2fb5d6d0e5e0d28001a1878cab775f262d5d5
Daniel Reina <dannyflatiron@gmail.com>
2019-11-07 22:46:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(array)
   list_of_welcome_message_and_room_assignment = []
   array.each_with_index do |name, room_number|
-    list_of_welcome_message_and_room_assignment << "Hello, #{name}! You'll be assigned to room #{room_number}!"
+    list_of_welcome_message_and_room_assignment << "Hello, #{name}! You'll be assigned to room #{room_number + 1}!"
   end
   list_of_welcome_message_and_room_assignment
 end
